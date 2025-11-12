60a2909115b687ccb1f3bd14107eab90a9e3966a
kala_allan <kalaitawa@gmail.com>
2025-11-12 23:07:11 +0100

    test_size divisé par 2 et ajout du random_state

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,6 +3,6 @@ from sklearn.model_selection import train_test_split
 def preprocess_data(data, testSize):
     """Nettoie, met en forme les données et prépare les ensembles de train et 
     de test"""
-    train, test = train_test_split(data, test_size = testSize)
+    train, test = train_test_split(data, test_size = testSize * 0.5, random_state = 42)
     return train, test
 
