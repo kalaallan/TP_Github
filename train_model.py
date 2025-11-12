a840bec08bbd64518f57831adda603f7cbaf2c71
kala_allan <kalaitawa@gmail.com>
2025-11-12 22:39:05 +0100

    test retiré et changement du commentaire

--- train_model.py
+++ train_model.py
@@ -1,7 +1,5 @@
-def train_model(train_X, train_y, test_X, model):
-    """entraine un modele ML de classification et retourne des predictions sur
-    le dataset de test"""
-    model.fit(train_X,train_y) 
-    prediction=model.predict(test_X)
-    return prediction
+def train_model(train_X, train_y, model):
+    """retourne le modèle entraîné"""
+    model.fit(train_X,train_y)
+    return model
 
